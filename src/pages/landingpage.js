d53be6b0bfefecec0d90aae68a48815b432a4ec0
Arthur Chardon <124440439+aestyus@users.noreply.github.com>
2024-03-14 10:04:12 +0100

    push

--- src/pages/landingpage.js
+++ src/pages/landingpage.js
@@ -6,12 +6,17 @@ import '../App.css';
 
 function LandingPage() {
 
+  const openDownloadsFolder = () => {
+    // Ouvrir un lien vers le dossier de téléchargements
+    window.open('file:///C:/Users/chard/Downloads');
+  };
+
   const { signOut, user } = useAuthenticator();
 
   return (
       <div className="App">
         <header className="App-header">
-          <h1>Panneau administrateur</h1>
+          <h1>Interface d'utilisation du boitier</h1>
           <p>Tu est connecté à un compte {user.attributes.nickname}</p>
           <p className="infos-de-connexion">Informations de connexion</p>
           <div className={'attributeContainer'}>
@@ -40,8 +45,30 @@ function LandingPage() {
                 <p className={'attribute'}>{user.attributes.nickname}</p>    
             </div>
           </div>
-          <button className={'sign-out'} onClick={signOut}>Sign out</button>
+          <button className={'sign-out-button'} onClick={signOut}>Sign out</button>
         </header>
+
+        <div className="App-consignes">
+          <h1>Suivez les étapes suivantes afin de recharger votre boitier</h1>
+          <div className={'tutoContainer'}>
+            <div className={'tutoSubContainer'}>
+                <p className={'tutoNumber'}>1 </p>
+                <p className={'tutoText'}>Eteignez votre boitier en basculant l'intérrupteur sur OFF</p>    
+            </div>
+            <div className={'tutoSubContainer'}>
+                <p className={'tutoNumber'}>2 </p>
+                <p className={'tutoText'}>Branchez le boitier à l'ordinateur grâce au cable prévu à cet effet</p>    
+            </div>
+            <div className={'tutoSubContainer'}>
+                <p className={'tutoNumber'}>3 </p>
+                <p className={'tutoText'}>Appuyez sur le boutton ci-dessous</p>
+                <div className={'divButton'}>
+                  <button className={'charger-button'} onClick={openDownloadsFolder}>Charger !</button>
+                </div>
+    
+            </div>
+          </div>
+        </div>
       </div>
   );
 }
